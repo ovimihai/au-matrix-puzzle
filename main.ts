d8aedd8c7dfc41e51719bdd78dc9441b0ed4c5b2
Anicai Ovidiu <ovi@0v1.ro>
2020-12-21 20:29:45 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+pins.digitalWritePin(DigitalPin.P1, 0)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P5) == 1) {
+        basic.showString("5")
+    } else if (pins.digitalReadPin(DigitalPin.P6) == 1) {
+        basic.showString("5")
+    } else {
+    	
+    }
+})
