16174bde338d357d6f3aecf9a170c3774e447dc9
Anicai Ovidiu <ovi@0v1.ro>
2020-12-21 20:31:06 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P5) == 1) {
         basic.showString("5")
     } else if (pins.digitalReadPin(DigitalPin.P6) == 1) {
-        basic.showString("5")
+        basic.showString("6")
     } else {
     	
     }
